615302ee6ad07c6ced10ff1b6f76b9583ff2a1fe
Tien Do <forever.love.wisdom@gmail.com>
2020-06-23 17:06:06 +0700

    Add Devise to test Action Caching

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/page_caching', to: 'page_caching#show'
+  root to: "home#index"
 end
