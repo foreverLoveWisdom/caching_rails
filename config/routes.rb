e9de2d47fba6e1ef1879d3a0eda5b2be926c1f3f
Tien Do <forever.love.wisdom@gmail.com>
2020-06-23 22:04:52 +0700

    Add seeding data for bike

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/page_caching', to: 'page_caching#show'
   root to: "home#index"
+  get '/home/show', to: 'home#show'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_23_100650) do
+ActiveRecord::Schema.define(version: 2020_06_23_145820) do
+
+  create_table "bikes", force: :cascade do |t|
+    t.string "model"
+    t.float "power"
+    t.float "torque"
+    t.integer "engine"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
